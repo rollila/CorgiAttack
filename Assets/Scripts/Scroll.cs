7e1ed8c02b1c1f4975b205e28ad93208f31f6150
smooshi <timereh@gmail.com>
2016-04-03 15:54:19 +0300

    bg stuff

---
+++ Assets/Scripts/Scroll.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scroll : MonoBehaviour
+{
+
+    public float scrollSpeed = 3f;
+    public GameObject wtf;
+    //public Renderer rndr = GetComponent<Renderer>();
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 offset = new Vector2(0, Time.time * scrollSpeed);
+        Vector2 offset2 = new Vector2((Time.time * scrollSpeed), 0);
+        wtf.GetComponent<Renderer>().material.mainTextureOffset = offset2;
+        //rndr.material.mainTextureOffset = offset;
+    }
+}
