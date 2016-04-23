0873265950c86a3ca551c950a0d31a1755a8b5c9
smooshi <timereh@gmail.com>
2016-04-23 14:01:38 +0300

    DESTRUCTOR

---
+++ Assets/Destructor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destructor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		//Debug.Log("DESTRUCTOR DESTROYS PUNY PLATFORM!!!!");
+		Destroy (other.gameObject);
+	}
+}
