d6fc9ccc4c39ca335aa6b50393329683667e89ea
smooshi <timereh@gmail.com>
2016-04-25 16:54:23 +0300

    XML

---
+++ Assets/Scripts/XML/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+using System.Xml.Serialization;
+
+public class Player {
+	[XmlAttribute("name")]
+	public string playerName;
+
+	[XmlElement("totalPoints")]
+	public int totalPoints;
+
+	[XmlElement("totalRounds")]
+	public int totalRounds;
+
+	[XmlElement("highScore")]
+	public int highScore;
+}
---
+++ Assets/Scripts/XML/StatHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+using UnityEngine;
+using System.Collections;
+
+public class StatHandler : MonoBehaviour {
+	private string playerStatsXMLfilepath = "Scripts/XML/playerStatStorage.xml";
+
+	private Player stat;
+
+	// Use this for initialization
+	void Start () {
+		var Stats = StatCollector.Load(Path.Combine(Application.dataPath, playerStatsXMLfilepath));
+		Debug.Log ("Loop through player events");
+		for (int i=0; i < Stats.Players.Length; i++) {
+			stat = Stats.Players [i];
+			Debug.Log (stat.playerName);
+			Debug.Log (stat.totalRounds);
+			Debug.Log (stat.totalPoints);
+			Debug.Log (stat.highScore);
+		}
+	}
+}
