4b4628cc5d5c6cb4711aa8c2e65c00d5a1b36f01
smooshi <timereh@gmail.com>
2016-04-28 17:13:52 +0300

    Maailman huonoin kyltti

---
+++ Assets/Scripts/NoDogsSign.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoDogsSign : MonoBehaviour {
+	private Animator anim;
+
+	void Awake() {
+		anim = GetComponent<Animator> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.layer == 10) { //player
+			//jos corgi dash -> hajoaa
+
+			//animation test:
+			anim.SetBool("corgiBreaksSign", true);
+			//animation["sign_break"].wrapMode = WrapMode.Once;
+			//animation.Play ("sign_break");
+			//Destroy();
+		}
+	}
+
+	//nope
+	IEnumerator Destroy() {
+		yield return new WaitForSeconds (0.1f); //pituus
+		Destroy(this.gameObject);
+	}
+}
