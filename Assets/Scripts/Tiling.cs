b25d1feb5a5ee3fb1916d1b8b2f58ed0d5974604
smooshi <timereh@gmail.com>
2016-05-03 14:12:00 +0300

    platforms

--- Assets/Scripts/Tiling.cs
+++ Assets/Scripts/Tiling.cs
@@ -14,7 +14,7 @@ public class Tiling : MonoBehaviour {
 		fixY = 7f; //en saanu muuten paikalleen
 		offCamera = 15f; //spawnaa ulompana
 
-		Instantiate(spawnables[Random.Range(0, 4)], new Vector3(transform.position.x, transform.position.y-fixY, 0-5), Quaternion.identity);
+		Instantiate(spawnables[Random.Range(0, 2)], new Vector3(transform.position.x, transform.position.y-fixY, 0-5), Quaternion.identity);
     }
 	
 	// Update is called once per frame
@@ -22,7 +22,7 @@ public class Tiling : MonoBehaviour {
         if (transform.position.x / movedX > 10)
         {
             Debug.Log("Spawn shit now");
-			Instantiate(spawnables[Random.Range(0, 4)], new Vector3(transform.position.x+offCamera, transform.position.y-fixY, 0-5), Quaternion.identity);
+			Instantiate(spawnables[Random.Range(0, 7)], new Vector3(transform.position.x+offCamera, transform.position.y-fixY, 0-5), Quaternion.identity);
             movedX++;
         }
 
